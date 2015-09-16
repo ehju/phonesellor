8cf7a56a1325ef91ca622ef5ac6d64f6aa7e8e4d
hello15369 <junejc0613@naver.com>
2015-09-16 16:16:39 +0000

    new box

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -53,7 +53,7 @@ class HomeController < ApplicationController
     end
     
   end
-  def last
+  def fourth
     @all=Phone.all
     #1.받은거를 기록하고 저장한다.
     $third=params[:num]
@@ -76,14 +76,37 @@ class HomeController < ApplicationController
     end
     
   end
+  def last
+    
+    $fourth=params[:num]
+    
+    @all=Phone.all
+    @selected=[]
+    @all.each do |x|
+      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))
+        @selected << x
+      end
+    end
+  #  남은수가 더 크다면
+    if (@selected.count >= 120)
+      #원래대로 120개를 뽑아낸다
+      @nonblank =(0..119).to_a.sample(120)
+      @random = @selected.sample(120)
+    else
+    # 남은수가 120보다 적으면 남은 수만큼을 nonblank로 뽑아낸다
+      @nonblank =(0..119).to_a.sample(@selected.count)
+      @random = @selected.sample(@selected.count)
+    end
+  end
 
   def result
+    
     $last=params[:num]
     
     @all=Phone.all
     @selected=[]
     @all.each do |x|
-      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $last.to_i)||(x.age == 1))
+      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))
         @selected << x
       end
     end
