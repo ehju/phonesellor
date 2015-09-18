325ee764a5583dab27ed373fbf34bae9f99eedc5
hello15369 <junejc0613@naver.com>
2015-09-18 06:30:59 +0000

    value slider

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -68,15 +68,15 @@ class HomeController < ApplicationController
     if (@selected.count >= 120)
       #원래대로 120개를 뽑아낸다
       @nonblank =(0..119).to_a.sample(120)
-      @random = @selected.sample(120)
+      @random = @selected.name.sample(120)
     else
     # 남은수가 120보다 적으면 남은 수만큼을 nonblank로 뽑아낸다
       @nonblank =(0..119).to_a.sample(@selected.count)
-      @random = @selected.sample(@selected.count)
+      @random = @selected.name.sample(@selected.count)
     end
     
   end
-  def last
+  def fifth
     
     $fourth=params[:num]
     
@@ -98,11 +98,32 @@ class HomeController < ApplicationController
       @random = @selected.sample(@selected.count)
     end
   end
+  def last
+    render layout: false
+    
+    $data=params[:num]
+    
+    @all=Phone.all
+    @selected=[]
+    @all.each do |x|
+      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))
+        @selected << x
+      end
+    end
+  #  남은수가 더 크다면
+    if (@selected.count >= 120)
+      #원래대로 120개를 뽑아낸다
+      @nonblank =(0..119).to_a.sample(120)
+      @random = @selected.sample(120)
+    else
+    # 남은수가 120보다 적으면 남은 수만큼을 nonblank로 뽑아낸다
+      @nonblank =(0..119).to_a.sample(@selected.count)
+      @random = @selected.sample(@selected.count)
+    end
+  end
 
   def result
-    
-    $last=params[:num]
-    
+    $val=params[:range]
     @all=Phone.all
     @selected=[]
     @all.each do |x|
