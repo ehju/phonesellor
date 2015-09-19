07ac73308a0c79fec026934c66ad0cdf1e896b5b
gekes <man8918@naver.com>
2015-09-19 10:08:04 +0000

    valuepass

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -92,20 +92,69 @@ class HomeController < ApplicationController
       #원래대로 120개를 뽑아낸다
       @nonblank =(0..119).to_a.sample(120)
       @random = @selected.uniq.sample(120)
+    els
     else
     # 남은수가 120보다 적으면 남은 수만큼을 nonblank로 뽑아낸다
       @nonblank =(0..119).to_a.sample(@selected.uniq.count)
       @random = @selected.uniq.sample(@selected.uniq.count)
     end
   end
+  
   def last
     
     $data=params[:num]
     
+    if ($data.to_i == 1)
+      $voice1 = 0
+      $voice2 = 300
+      $data1 = 7
+      $data2 = 100
+    elsif ($data.to_i == 2)
+      $voice1 = 300
+      $voice2 = 800
+      $data1 = 7
+      $data2 = 100
+    elsif ($data.to_i == 3)
+      $voice1 = 800
+      $voice2 = 3000
+      $data1 = 7
+      $data2 = 100
+    elsif ($data.to_i == 4)
+      $voice1 = 0
+      $voice2 = 300
+      $data1 = 3
+      $data2 = 7
+    elsif ($data.to_i == 5)
+      $voice1 = 300
+      $voice2 = 800
+      $data1 = 3
+      $data2 = 7
+    elsif ($data.to_i == 6)
+      $voice1 = 800
+      $voice2 = 3000
+      $data1 = 3
+      $data2 = 7
+    elsif ($data.to_i == 7)
+      $voice1 = 0
+      $voice2 = 300
+      $data1 = 0
+      $data2 = 3
+    elsif ($data.to_i == 8)
+      $voice1 = 300
+      $voice2 = 800
+      $data1 = 0
+      $data2 = 3
+    elsif ($data.to_i == 9)
+      $voice1 = 800
+      $voice2 = 3000
+      $data1 = 0
+      $data2 = 3
+    end
+    
     @all=Phone.all
     @selected=[]
     @all.each do |x|
-      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))
+      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))&&(x.voice >= $voice1.to_i)&&(x.voice <= $voice2.to_i)&&(x.data >= $data1.to_i)&&(x.data <= $data2.to_i)
         @selected << x.name
       end
     end
@@ -124,10 +173,11 @@ class HomeController < ApplicationController
 
   def graph
     $val=params[:range]
+    
     @all=Phone.all
     @selected=[]
     @all.each do |x|
-      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))
+      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))&&(x.voice >= $voice1.to_i)&&(x.voice <= $voice2.to_i)&&(x.data >= $data1.to_i)&&(x.data <= $data2.to_i)&&(x.price <= $val.to_i)
         @selected << x
       end
     end
@@ -138,8 +188,8 @@ class HomeController < ApplicationController
     @all=Phone.all
     @selected=[]
     @all.each do |x|
-      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))
-        @selected << x.name
+      if (x.company == $first.to_i)&&(x.payment == $second.to_i)&&(x.service == $third.to_i)&&((x.age == $fourth.to_i)||(x.age == 1))&&(x.voice >= $voice1.to_i)&&(x.voice <= $voice2.to_i)&&(x.data >= $data1.to_i)&&(x.data <= $data2.to_i)&&(x.price <= $val.to_i)
+        @selected << x
       end
     end
   #  남은수가 더 크다면
